5e6214c62cf3b857a9aa881ac3de07ba95ca8bc4
Stephen Joe <sjonany@gmail.com>
2014-02-27 11:39:32 -0800

    Added sample from multinomial code + tested

--- src/util/StatUtil.java
+++ src/util/StatUtil.java
@@ -1,8 +1,37 @@
 package util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StatUtil {
+	public static void main(String[] args) {
+		Map<Integer, Integer> tally = new HashMap<Integer, Integer> ();
+		double[] weights = {1, 2, 3};
+		for(int i = 0; i < 6000; i++) {
+			addToTally(tally, sampleMultinomial(weights), 1);
+		}
+		System.out.println(tally);
+	}
+	
+	/**
+	 * O(N) multinomial sampler
+	 * @return zero-indexed i, with probability weights[i] / sum{weights}
+	 */
+	public static int sampleMultinomial(double[] weights) {
+		double[] sum = new double[weights.length];
+		sum[0] = weights[0];
+		for(int i = 1; i < weights.length; i++) {
+			sum[i] = weights[i] + sum[i-1];
+		}
+		
+		double rand = Math.random() * sum[sum.length-1];
+    for (int i = 0; i < weights.length; i++) {
+        if (rand <= sum[i])
+           return i;
+    }
+    return weights.length-1;
+	}
+	
 	public static <K> void addToTally(Map<K, Integer> tally, K key, int val) {
 		if(!tally.containsKey(key)) {
 			tally.put(key, val);
@@ -53,8 +82,5 @@ public class StatUtil {
 			this.fn += other.fn;
 			this.tn += other.tn;
 		}
-	}
-	public static void main(String[] args){
-		System.out.println(Stemmer.getInstance().getStemmedForm("as"));
 	}
 }
